311c2b53e3d6bb35152af0bdd57f6c96976e52c5
gillmpadon <gillbertmpadon@gmail.com>
2024-02-08 15:08:07 +0800

    fastCommit

--- request.py
+++ request.py
@@ -11,29 +11,40 @@ def getOne(id):
         if item['queueId'] == id:
           data = item['queueStatus']
     return data
-def getOneRowWaiting(terminalTransactionType):
+def getTerminal():
+    fetchTerminal = getOneTerminal()
+    if(len(fetchTerminal.keys()) == 0):
+        return None
+    else:
+        return fetchTerminal.get('transactionType')
+def getOneRowWaiting():
     data = []
+    transactionType = getTerminal()
     fetch = requests.get(f"{config.get('Configuration', 'URL')}/queue")
     fetch = fetch.json()
     for item in fetch:
-        if item['queueStatus'] != 'accommodated' and item['transactionType'] == terminalTransactionType:
+        if item['queueStatus'] != 'accommodated' and item['transactionType'] == transactionType:
             data.append(item)
     if(len(data)>0):
         data_sorted = sorted(data, key=lambda x: x.get('queueId', ''))
-        print(type(data_sorted))
         return data_sorted[0]
-    
     return {}
 
-print(getOneRowWaiting('payment'))
+def getOneTerminal():
+    fetch = requests.get(f"{config.get('Configuration', 'URL')}/terminal/{config.get('Configuration', 'TERMINAL')}")
+    fetch = fetch.json()
+    return fetch
+
 def updateData(id):
     getOneQueue = getOne(id)
     status ="ongoing" if getOneQueue == "waiting" else "accommodated"
-    terminal = (f"{config.get('Configuration', 'TERMINAL')}")
+    terminal = getOneTerminal()
+    terminal = terminal.get('terminalName')
     data = {"queueId": int(id), "queueStatus": status,"terminal":terminal}
     headers = {'Content-Type': 'application/json'}
     fetch = requests.patch(f"{config.get('Configuration', 'URL')}/queue", json=data, headers=headers)
     result = fetch.json()
     message = f"{result['name']} is now {result['queueStatus']}" if fetch.status_code==200 else "Something Went Wrong"
     return message
+
 
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import os, configparser, sys
 from PySide6 import QtWidgets, QtGui
-from request import getOneRowWaiting, updateData
+from request import getOneRowWaiting, updateData, getOneTerminal
 config = configparser.ConfigParser()
+
+
 config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 def notified(status, message):
     w = QtWidgets.QWidget()
@@ -13,30 +15,25 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     def __init__(self, icon, parent=None):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
         self.currentQueueId = 0
+        self.transactionType = ''
         self.setToolTip('QMS SYSTEM TRAY')
 
         self.menu = QtWidgets.QMenu(parent)
         self.setContextMenu(self.menu)
         self.activated.connect(self.onTrayIconActivated)
-
-    def onTrayIconActivated(self, reason):
-        # if reason == QtWidgets.QSystemTrayIcon.Trigger:
+    
+    def onTrayIconActivated(self):
         self.refreshMenu()
 
     def refreshMenu(self):
-        # Clear the existing menu
         self.menu.clear()
         self.menu.setStyleSheet("font-size: 24px")
-        # Add new items
-        queueCustomer = getOneRowWaiting('payment')
+        queueCustomer = getOneRowWaiting()
         if(len(queueCustomer.keys())==0):
             self.menu.addAction(f"No List")
         else:
             name, queueId, queueStatus = queueCustomer.get('name'), queueCustomer.get('queueId'), queueCustomer.get('queueStatus')
             self.menu.addAction(f"{name} {queueStatus}")
-            # open_app.triggered.connect(lambda n=name, q=queueId: self.sendRequest(n, q))
-            # open_app.hovered.connect(lambda: self.setHoverIcon(open_app, "assets/logo.png"))
-            # Add exit action back to the menu
             next = self.menu.addAction(f'{ "Next" if queueStatus=="waiting" else "Finish"}')
             next.setIcon(QtGui.QIcon(f'assets/{ "next" if queueStatus=="waiting" else "finish"}.png'))
             next.triggered.connect(lambda n=name, q=queueId: self.sendRequest(n, q))
