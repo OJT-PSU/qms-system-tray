7e35ec11c76bd5e702256444fa8d5730e4fbf648
gillmpadon <gillbertmpadon@gmail.com>
2024-02-13 11:52:21 +0800

    fully functional and with exe

--- test.py
+++ test.py
@@ -2,12 +2,10 @@ import os, configparser, sys
 from PySide6 import QtWidgets, QtGui
 from request import getOneRowWaiting, updateData, getOneTerminal
 config = configparser.ConfigParser()
-
-
 config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 def notified(status, message):
     w = QtWidgets.QWidget()
-    tray_icon = SystemTrayIcon(QtGui.QIcon("assets/logo.png"), w)
+    tray_icon = SystemTrayIcon(QtGui.QIcon("logo.png"), w)
     tray_icon.show()
     tray_icon.showMessage(status, message)
 
@@ -35,12 +33,12 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
             name, queueId, queueStatus = queueCustomer.get('name'), queueCustomer.get('queueId'), queueCustomer.get('queueStatus')
             self.menu.addAction(f"{name} {queueStatus}")
             next = self.menu.addAction(f'{ "Next" if queueStatus=="waiting" else "Finish"}')
-            next.setIcon(QtGui.QIcon(f'assets/{ "next" if queueStatus=="waiting" else "finish"}.png'))
+            next.setIcon(QtGui.QIcon(f'{ "next" if queueStatus=="waiting" else "finish"}.png'))
             next.triggered.connect(lambda n=name, q=queueId: self.sendRequest(n, q))
                 
         # self.menu.setStyleSheet(open("style.css", "r").read())
         exit_ = self.menu.addAction("Exit")
-        exit_.setIcon(QtGui.QIcon("assets/exit.png"))
+        exit_.setIcon(QtGui.QIcon("exit.png"))
         exit_.triggered.connect(lambda: sys.exit())
 
     def setHoverIcon(self, action, hover_icon_path):
@@ -53,8 +51,8 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
 def main():
     app = QtWidgets.QApplication(sys.argv)
     w = QtWidgets.QWidget()
-    tray_icon = SystemTrayIcon(QtGui.QIcon("assets/logo.png"), w)
-    tray_icon.show()
+    tray_icon = SystemTrayIcon(QtGui.QIcon(os.path.join(os.path.dirname(__file__), "systemTray.ico")), w)
+    tray_icon.setVisible(True)
     tray_icon.showMessage('Welcome',config.get('Configuration', 'CASHIER_NAME'))
     sys.exit(app.exec())
 
